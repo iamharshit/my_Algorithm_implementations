892c3fbb140e9e5807e4d214f588315a26466b3c
iamharshit <harshitagarwal37@gmail.com>
2016-05-11 23:04:50 +0530

    finding gcd in c++

---
+++ number_theory_ALGOs/gcd.cpp
@@ -0,0 +1,23 @@
+// in c++ we can directly do `__gcd(a,b)` for finding gcd of a,b
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int x,int y){
+    if(y%x==0) return x;
+    else return gcd(y%x,x);
+}
+
+int main()
+{
+	int a; cin>>a;
+	int b; cin>>b;
+
+	int big,small;
+	big=(a>b)?a:b;
+	small=(a>b)?b:a;
+
+	int the_gcd=gcd(big,small);
+
+  return 0;
+}
