6ec6436a8769950e029f6147446e5670f1a2aadc
iamharshit <harshitagarwal37@gmail.com>
2016-05-18 17:45:20 +0530

    very basic java template

---
+++ handy.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
